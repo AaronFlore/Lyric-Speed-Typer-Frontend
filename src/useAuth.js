813a845934916baba430eace876a7c00f1e569c6
Aaron Floreani <aaronfloreani@gmail.com>
2023-03-02 00:38:35 -0800

    change

--- src/useAuth.js
+++ src/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("https://lyricspeedtyper-api.onrender.com/login", {
+      .post("https://lyric-speed-typer-backend-production.up.railway.app/login", {
         code,
       })
       .then(res => {
@@ -26,7 +26,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post("https://lyricspeedtyper-api.onrender.com/refresh", {
+        .post("https://lyric-speed-typer-backend-production.up.railway.app/refresh", {
           refreshToken,
         })
         .then(res => {
